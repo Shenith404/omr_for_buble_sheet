4a471fdf3030eebbd65870886f0b278736c8d5da
Thisaru Dissanayake <118095399+ThisaruDissanayake@users.noreply.github.com>
2025-05-18 16:28:50 +0530

    try  to save RGB splitted blocks

--- utils.py
+++ utils.py
@@ -143,3 +143,98 @@ def saveImages(answerBlocks, ImageName, boxNumber):
             file_path = os.path.join(save_path_cross_Images, filename)
 
         cv2.imwrite(file_path, block)
+
+
+
+
+# import os
+# import uuid
+# import time
+# import numpy as np
+# import cv2
+
+# def stackImages(imgArray, scale, labels=[]):
+#     rows = len(imgArray)
+#     cols = len(imgArray[0])
+#     rowsAvailable = isinstance(imgArray[0], list)
+#     width = imgArray[0][0].shape[1]
+#     height = imgArray[0][0].shape[0]
+#     newWidth = int(width * scale)
+#     newHeight = int(height * scale)
+    
+#     if rowsAvailable:
+#         for x in range(rows):
+#             for y in range(cols):
+#                 imgArray[x][y] = cv2.resize(imgArray[x][y], (newWidth, newHeight))
+#                 if len(imgArray[x][y].shape) == 2:
+#                     imgArray[x][y] = cv2.cvtColor(imgArray[x][y], cv2.COLOR_GRAY2BGR)
+#         hor = [np.hstack(imgArray[x]) for x in range(rows)]
+#         ver = np.vstack(hor)
+#     else:
+#         for x in range(rows):
+#             imgArray[x] = cv2.resize(imgArray[x], (newWidth, newHeight))
+#             if len(imgArray[x].shape) == 2:
+#                 imgArray[x] = cv2.cvtColor(imgArray[x], cv2.COLOR_GRAY2BGR)
+#         ver = np.hstack(imgArray)
+    
+#     return ver
+
+# def rectContour(contours):
+#     rectCon = []
+#     for i in contours:
+#         area = cv2.contourArea(i)
+#         if area > 50:
+#             peri = cv2.arcLength(i, True)
+#             approx = cv2.approxPolyDP(i, 0.02 * peri, True)
+#             if len(approx) == 4:
+#                 rectCon.append(i)
+#     rectCon = sorted(rectCon, key=cv2.contourArea, reverse=True)
+#     return rectCon
+
+# def getCornerPoints(cont):
+#     peri = cv2.arcLength(cont, True)
+#     return cv2.approxPolyDP(cont, 0.02 * peri, True)
+
+# def reorder(points):
+#     points = points.reshape((4, 2))
+#     newPoints = np.zeros((4, 1, 2), np.int32)
+#     add = points.sum(1)
+#     newPoints[0] = points[np.argmin(add)]
+#     newPoints[3] = points[np.argmax(add)]
+#     diff = np.diff(points, axis=1)
+#     newPoints[1] = points[np.argmin(diff)]
+#     newPoints[2] = points[np.argmax(diff)]
+#     return newPoints
+
+# def verticalSplitBoxes(img):
+#     cols = np.hsplit(img, 5)
+#     boxes = []
+#     for c in cols:
+#         rows = np.vsplit(c, 10)
+#         for box in rows:
+#             boxes.append(box)
+#     return boxes
+
+# def getAnswerBlocks(img):
+#     img = cv2.resize(img, (350, 50))
+#     return np.hsplit(img, 7)
+
+# def saveImages(answerBlocks, imageName, boxNumber):
+#     cross_path = r"D:\University\fyp\mcq_test_1\cross_Images"
+#     empty_path = r"D:\University\fyp\mcq_test_1\empty_Images"
+#     os.makedirs(cross_path, exist_ok=True)
+#     os.makedirs(empty_path, exist_ok=True)
+
+#     for i, block in enumerate(answerBlocks):
+#         gray_block = cv2.cvtColor(block, cv2.COLOR_BGR2GRAY)
+#         totalPixels = cv2.countNonZero(gray_block)
+#         unique_id = str(uuid.uuid4())[:8]
+#         timestamp = int(time.time())
+#         filename = f"{imageName}_{boxNumber + i}_{timestamp}_{unique_id}.jpg"
+
+#         if totalPixels < 550:
+#             file_path = os.path.join(empty_path, filename)
+#         else:
+#             file_path = os.path.join(cross_path, filename)
+        
+#         cv2.imwrite(file_path, block)
